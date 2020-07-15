64d1090a61bc2fadf4bcd55496dbd5f83b4678ab
ajay daram <ajayforchrist777@gmail.com>
2020-07-16 00:38:40 +0530

    learn to add styles

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}> Here are the some boxes of different colors</Text>
+      </View>
+      <View style={styles.cyanBox}>
+        <Text style={styles.boxText}>Cyan: #2aa198</Text>
+      </View>
+      <View style={styles.blueBox}>
+        <Text style={styles.boxText}>Blue: #268bd2</Text>
+      </View>
+      <View style={styles.magentaBox}>
+        <Text style={styles.boxText}>Magneta: #d33682</Text>
+      </View>
+      <View style={styles.orangeBox}>
+        <Text style={styles.boxText}>Orange: #cb4b16</Text>
+      </View>
+    </SafeAreaView>
   );
-}
-
+}; 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 40,
   },
-});
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  cyanBox: {
+    backgroundColor: "#2aa198",
+    padding: 10,
+    textAlign: "center",
+    marginHorizontal: 5,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  boxText: {
+     color: "white",
+     fontWeight: "bold",
+  },
+  blueBox: {
+    backgroundColor: "#268bd2",
+    padding: 10,
+    textAlign: "center",
+    marginHorizontal: 5,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  magentaBox: {
+    backgroundColor: "#d33682",
+    padding: 10,
+    textAlign: "center",
+    marginHorizontal: 5,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  orangeBox: {
+    backgroundColor: "#cb4b16",
+    padding: 10,
+    textAlign: "center",
+    marginHorizontal: 5,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+})
+export default App;
